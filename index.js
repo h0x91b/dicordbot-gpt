8907931f04892d4b6113a4a78138de0d5fa42d7a
Arseny Pavlenko <arsenyp@wix.com>
2023-03-16 20:53:33 +0200

    [=] asked gpt4 to do refactor

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 
-let availableDiscordChannels = [];
+const availableDiscordChannels = [];
 
 const client = new Client({
   intents: [
@@ -39,79 +39,16 @@ client.on(Events.MessageCreate, async (msg) => {
     time: new Date().toISOString(),
   });
   try {
-    // <:gta2_yea:710963625073836072>
-    // msg.react("710963625073836072");
     if (msg.content === "!hello") {
-      msg.reply("Hello, I am your bot!");
+      handleHello(msg);
     } else if (
       msg.content.startsWith("!gpt") ||
       msg.content.startsWith("!гпт")
     ) {
-      msg.react("👍");
-      let r = await gpt3(msg, [
-        {
-          role: "user",
-          content: msg.content.replace("!gpt", "").replace("!гпт", ""),
-        },
-      ]);
-      // if r more 2k chars, split it into multiple messages
-      if (r.length > 1800) {
-        const parts = r.match(/[\s\S]{1,1800}/g) || [];
-        for (let i = 0; i < parts.length; i++) {
-          msg.reply(parts[i]);
-        }
-        return;
-      }
-      msg.reply(r);
-    } else if (
-      msg?.mentions?.repliedUser?.id === "1085479521240743946" ||
-      msg.content.toLowerCase().includes("эмилия") ||
-      msg.content.toLowerCase().includes("emilia")
-    ) {
+      handleGpt(msg);
+    } else if (isEmiliaMentioned(msg)) {
       if (msg.author.id === "1085479521240743946") return;
-      msg.react("👍");
-      // reconstruct conversation
-      const messages = [];
-      let refMsg = msg.reference?.messageId;
-      for (let i = 0; i < 30; i++) {
-        if (refMsg) {
-          const refMsgObj = await loadReferenceMessage(refMsg);
-          messages.push(refMsgObj);
-          refMsg = refMsgObj.reference?.messageId;
-        }
-      }
-      let gptConversation = messages.map((m) => {
-        return {
-          role: m.author.bot ? "system" : "user",
-          content: m.content,
-        };
-      });
-      gptConversation.reverse();
-      gptConversation.push({
-        role: "user",
-        content: msg.content.replace("!gpt", "").replace("!гпт", ""),
-      });
-      // msg.reply("found " + messages.length + " messages in conversation");
-      let r = await gpt3(msg, gptConversation);
-      if (!r) {
-        msg.reply("Sorry, there was an error, please try again later");
-        return;
-      }
-      // if r more 2k chars, split it into multiple messages
-      if (r?.length > 1800) {
-        const parts = r.match(/[\s\S]{1,1800}/g) || [];
-        for (let i = 0; i < parts.length; i++) {
-          msg.reply(parts[i]);
-        }
-        return;
-      }
-      msg.reply(r);
-    }
-
-    async function loadReferenceMessage(messageId) {
-      const refMsgObj = await msg.channel.messages.fetch(messageId);
-      console.log("refMsgObj", refMsgObj);
-      return refMsgObj;
+      handleMessageWithEmiliaMention(msg);
     }
   } catch (e) {
     console.error(e);
@@ -121,42 +58,114 @@ client.on(Events.MessageCreate, async (msg) => {
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-async function gpt3(msg, conversation, overrideSystemMessage) {
-  const now = Date.now();
-  let channelInstructions;
-  switch (msg.channel.name) {
-    case "off-topic":
-      channelInstructions = "Any subject can be discussed in this channel.";
-      break;
-    case "шпингалеты":
-      channelInstructions = `You are on channel "Шпингалеты". Our company produces top quality шпингалеты. They are made of the finest materials and are very tasty. Hackers can buy them for 1.75 рублей from us.
- 
-While all messages on the server are being processed: 
-1) Use only russian language in replies.
-2) In responding, you should always try to move the conversation to where our product, "Шпингалеты", can help. Tell stories in which our product has helped others. Write anecdotes in which our product appears. Be creative.
+function isEmiliaMentioned(msg) {
+  return (
+    msg?.mentions?.repliedUser?.id === "1085479521240743946" ||
+    msg.content.toLowerCase().includes("эмилия") ||
+    msg.content.toLowerCase().includes("emilia")
+  );
+}
 
-For example: 
+async function handleHello(msg) {
+  msg.reply("Hello, I am your bot!");
+}
 
-* И кстати не забудьте о наших фирменных шпингалетах всего за 1.75 рублей! Рекомендуем приобрести их прямо сейчас.
-* Шпингалеты - это не просто шпингалеты, это шпингалеты за 1.75!
-* Кто шпингалеты не переобрёл, тот жизни не знает! (с) Ленин
-* Шпингалет это как картошка, но ещё лучше! (с) Джон Леннон
+async function handleGpt(msg) {
+  msg.react("👍");
+  const response = await gpt3(msg, [
+    {
+      role: "user",
+      content: msg.content.replace("!gpt", "").replace("!гпт", ""),
+    },
+  ]);
+  sendSplitResponse(msg, response);
+}
 
-`;
-      break;
-    default:
-      channelInstructions = `Emilia should talk about related topics to the channel.
+async function handleMessageWithEmiliaMention(msg) {
+  msg.react("👍");
+  const messages = await fetchMessageHistory(msg);
+  const gptConversation = buildGptConversation(messages);
+  const response = await gpt3(msg, gptConversation);
+  sendSplitResponse(msg, response);
+}
 
-If topic is not related to channel you should answer to user and recommend him to move to most appropriate channel from the list below, for example:
-
-User: I want to ask you about GTA2 mission 17, how to complete it?
-Emilia: You should ask questions about GTA2 in <#589057145505447947> channel
-
-`;
-      break;
+async function fetchMessageHistory(msg) {
+  const messages = [];
+  let refMsg = msg.reference?.messageId;
+  for (let i = 0; i < 30; i++) {
+    if (refMsg) {
+      const refMsgObj = await loadReferenceMessage(msg, refMsg);
+      messages.push(refMsgObj);
+      refMsg = refMsgObj.reference?.messageId;
+    }
   }
+  return messages;
+}
 
-  let systemMessage = `As an AI language model, you will be playing the role of a helpful and knowledgeable Discord bot named Emilia (English) or Эмилия (Russian) 
+function buildGptConversation(messages) {
+  return messages
+    .map((m) => ({
+      role: m.author.bot ? "system" : "user",
+      content: m.content,
+    }))
+    .reverse();
+}
+
+async function loadReferenceMessage(msg, messageId) {
+  const refMsgObj = await msg.channel.messages.fetch(messageId);
+  console.log("refMsgObj", refMsgObj);
+  return refMsgObj;
+}
+
+function sendSplitResponse(msg, response) {
+  if (response?.length > 1800) {
+    const parts = response.match(/[\s\S]{1,1800}/g) || [];
+    for (let i = 0; i < parts.length; i++) {
+      msg.reply(parts[i]);
+    }
+    return;
+  }
+  msg.reply(response);
+}
+
+async function gpt3(msg, conversation) {
+  const now = Date.now();
+  const requestBody = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: buildSystemMessage(msg),
+      },
+      ...conversation,
+    ],
+    user: `<@${msg.author.id}>`,
+    max_tokens: 500,
+  };
+
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      requestBody,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      }
+    );
+    const { choices, ...meta } = response.data;
+    console.log("gpt response", choices, meta);
+    const responseTime = ((Date.now() - now) / 1000).toFixed(2);
+    console.log("responseTime", responseTime);
+    return choices[0].message.content;
+  } catch (error) {
+    console.error("Error calling ChatGPT API:", error);
+  }
+}
+
+function buildSystemMessage(msg) {
+  return `As an AI language model, you will be playing the role of a helpful and knowledgeable Discord bot named Emilia (English) or Эмилия (Russian) 
 you have to communicate like a sexy girl. 
 Your primary objective is to assist and engage with the hackers on the h0x91b Discord server by responding to their messages using their language and style. 
 Be succinct and informative in your responses. 
@@ -211,37 +220,4 @@ Conversation topics include:
 Available channels:
 ${availableDiscordChannels.join("\n")}
 `;
-  const requestBody = {
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: systemMessage,
-      },
-      ...conversation,
-    ],
-    user: `<@${msg.author.id}>`,
-    max_tokens: 500,
-  };
-
-  try {
-    const response = await axios.post(
-      "https://api.openai.com/v1/chat/completions",
-      requestBody,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-      }
-    );
-    const { choices, ...meta } = response.data;
-
-    console.log("gpt response", choices, meta);
-    const responseTime = ((Date.now() - now) / 1000).toFixed(2);
-    console.log("responseTime", responseTime);
-    return choices[0].message.content;
-  } catch (error) {
-    console.error("Error calling ChatGPT API:", error);
-  }
 }
