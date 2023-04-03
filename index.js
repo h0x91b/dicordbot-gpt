47d23e7e79c082a9fc6955b28daae843537a51d4
Arseny Pavlenko <arsenyp@wix.com>
2023-04-03 20:45:10 +0300

    [=] grammar bot

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const authorsToAllowGPT4 = [
 ];
 const fixGrammarUsers = [
   "309119244979798016", // Wlastas
-  "405507382207315978", // h0x91b
+  // "405507382207315978", // h0x91b
 ];
 
 client.on("ready", async () => {
